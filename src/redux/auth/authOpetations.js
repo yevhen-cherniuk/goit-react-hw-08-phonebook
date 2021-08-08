06700caa80c5fe0b03d59779756f25ec2b3fd5be
yevhen-cherniuk <chernuy1@gmail.com>
2021-08-08 22:03:06 +0400

    fix

--- src/redux/auth/authOpetations.js
+++ src/redux/auth/authOpetations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authActions from "./authActions";
 
-axios.defaults.baseURL = "https://goit-phonebook-api.herokuapp.com";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
 const token = {
   set(token) {
